0335097d8121d2df8e040323d74367299c162492
Ankit Sharma <ankitsharma290101@gmail.com>
2023-10-06 17:02:11 +0530

    Navbar+ Routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,60 @@
+import { BrowserRouter, Link, Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
-import {Navbar,Footer,Main} from "./components";
+import { CryptoDetails, Cryptocurrencies, Exchanges, HomePage, Navbar, News } from "./components";
+import { Layout, Space } from "antd";
+import Typography from "antd/es/typography/Typography";
+
 function App() {
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Navbar />,
+      children:[
+        {
+          path: "/",
+          element: <HomePage />,
+        },
+        {
+          path: "exchanges",
+          element: <Exchanges />,
+        },
+        {
+          path: "cryptocurrencies",
+          element: <Cryptocurrencies />,
+        },
+        {
+          path: "crypto/:coinId",
+          element: <CryptoDetails />,
+        },
+        {
+          path: "news",
+          element: <News />,
+        }
+
+      ]
+    },
+    
+  ]);
+
   return (
-    <div className="App">
-      <div className="navbar">
-        <Navbar />
+      <div className="App">
+      <div className="containe">
+        <div className="main">
+         <RouterProvider router={appRouter}/>
+        </div></div>
+        <div className="footer">
+        <Typography.Title level={5} style={{color:'black',textAlign:'center'}}>
+          Rupadravya<br/>
+          All Rights Reserved
+        </Typography.Title>
+        {/* <Space>
+          <Link to="/">Home</Link>
+          <Link to="exchanges">Exchanges</Link>
+          <Link to="news">News</Link>
+        </Space> */}
+        </div>
       </div>
-      <div className="main">
-        <Main />
-      </div>
-      <div className="footer">
-        <Footer />
-      </div>
-    </div>
+    
   );
 }
 
